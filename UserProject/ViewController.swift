cda8aa879a70f8658574de6c1499a999434da985
Connor Kuehnle <connor.kuehnle@housecallpro.com>
2018-08-08 12:45:17 -0700

    Add files

--- UserProject/ViewController.swift
+++ UserProject/ViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        fetchData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func fetchData() {
+        Alamofire.request("https://jsonplaceholder.typicode.com/users",
+                          method: .get)
+            .responseJSON { response in
+                guard let value = response.result.value as?  [Dictionary<String, Any>] else{
+                    return
+                }
+                print(value)
+        }
+    }
 }
 
